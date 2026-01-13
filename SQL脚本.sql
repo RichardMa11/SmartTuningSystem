3aca76dc7db1567a7cde455312e9b2f41710e8f5
Richard Ma <Administrator@LENOVO-PC>
2026-01-13 14:34:09 +0800

    完善delete脚本

--- SQL脚本.sql
+++ SQL脚本.sql
@@ -2,11 +2,13 @@ DELETE FROM [SmartTuningSystemDB].[dbo].[DeviceInfo]
 
 DELETE FROM [SmartTuningSystemDB].[dbo].[DeviceInfoDetail]
 
+DELETE FROM [SmartTuningSystemDB].[dbo].[InspectionLock]
+
 DELETE FROM [SmartTuningSystemDB].[dbo].[Logs]
 
-DELETE FROM [SmartTuningSystemDB].[dbo].[Product]
-
 DELETE FROM [SmartTuningSystemDB].[dbo].[RoleMenu]
+
+DELETE FROM [SmartTuningSystemDB].[dbo].[RolePermission]
 
 DELETE FROM [SmartTuningSystemDB].[dbo].[Roles]
 
